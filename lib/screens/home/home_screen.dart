1c290a31f90935513d33be317262239dd5385606
Emad hanna <emadhanna565@gmail.com>
2023-07-07 22:04:16 +0300

    + design popular movie and get data from api + design release movie and get data from api + design recommend movie and get data from api

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/shared/style/component/movie_list.dart';
+import 'package:movie_app/shared/style/component/movie_popular_item.dart';
+import 'package:movie_app/shared/style/component/new_release_item.dart';
+import 'package:movie_app/shared/style/component/recommend_item.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SafeArea(child: SizedBox()),
+          Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: MoviePopularItem()),
+          SizedBox(
+            height: 20,
+          ),
+          MoviesList(
+              listItem: NewReleaseItem(), title: 'New Releases', height: 0.3),
+          SizedBox(
+            height: 20,
+          ),
+          MoviesList(
+              listItem: RecommendItem(), title: 'Recommended', height: 0.42),
+        ],
+      ),
     );
   }
 }
